d6699eba35bd577045298dd338202cfa89d25399
David Bucciarelli <dade916@gmail.com>
2016-03-17 16:28:40 +0100

    More work on docker support

--- docker-luxrender/build.sh
+++ docker-luxrender/build.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
 cd /tmp
+echo " *** Cloning https://bitbucket.org/luxrender/linux ***"
 hg clone https://bitbucket.org/luxrender/linux linux
 cd linux
 hg update docker_integration
 
 # Clone Lux and LuxRays repositories
+echo " *** Cloning https://bitbucket.org/luxrender/lux ***"
 hg clone https://bitbucket.org/luxrender/lux lux
 cd lux
+echo " *** Cloning https://bitbucket.org/luxrender/luxrays ***"
 hg clone https://bitbucket.org/luxrender/luxrays luxrays
 cd ..
-
-# Remove mercurial and python2.7
-apt-get remove -y mercurial
-apt-get autoremove -y
-
-# Install python 3.4
-apt-get update
-apt-get install -y python3.4 libpython3.4-dev
-# Set python3.4 as default python
-update-alternatives --install /usr/bin/python python /usr/bin/python3.4 1
 
 # Build everything
 ./build-64-sse2 lux
