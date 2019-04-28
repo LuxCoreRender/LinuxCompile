a741b946513ee7a2a11abd8fd08ace668e8445ed
David Bucciarelli <dade916@gmail.com>
2019-04-28 17:17:38 +0200

    Fixed LuxMark compilation

--- distfiles/qt-install.sh
+++ distfiles/qt-install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cp -av 5.12.2/gcc_64/* $1
+cp -av 5.12.2/gcc_64/bin $1
+cp -av 5.12.2/gcc_64/include $1
+cp -av 5.12.2/gcc_64/lib $1
+cp -av 5.12.2/gcc_64/mkspecs $1
+cp -av 5.12.2/gcc_64/plugins $1
