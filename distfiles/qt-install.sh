a8c1abc88f85d435378f4c9f8ccbaecdb54ee840
David Bucciarelli <dade916@gmail.com>
2019-10-07 15:26:46 +0200

    Release LuxMark v4.0alpha0

--- distfiles/qt-install.sh
+++ distfiles/qt-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp -av 5.12.2/gcc_64/bin $1
-cp -av 5.12.2/gcc_64/include $1
-cp -av 5.12.2/gcc_64/lib $1
-cp -av 5.12.2/gcc_64/mkspecs $1
-cp -av 5.12.2/gcc_64/plugins $1
+mkdir $1/qt5-v5.12.2
+cp -av 5.12.2/gcc_64/bin $1/qt5-v5.12.2
+cp -av 5.12.2/gcc_64/include $1/qt5-v5.12.2
+cp -av 5.12.2/gcc_64/lib $1/qt5-v5.12.2
+cp -av 5.12.2/gcc_64/mkspecs $1/qt5-v5.12.2
+cp -av 5.12.2/gcc_64/plugins $1/qt5-v5.12.2
