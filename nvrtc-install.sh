87a2f8448fa528fe88480dd687d3e25b84ce5f4d
Simon Wendsche <simon.wendsche@gmx.de>
2020-08-04 20:55:10 +0200

    patch rpath of libnvrtc shared libraries

--- nvrtc-install.sh
+++ nvrtc-install.sh
@@ -7,3 +7,7 @@ mkdir -p $INCLUDE_INSTALL_DIR
 mkdir -p $LIBRARY_INSTALL_DIR
 
 cp -avr targets/x86_64-linux/lib/libnvrtc* $LIBRARY_INSTALL_DIR
+
+# Fix missing rpath, so one shared lib can find the other when it is loaded by BlendLuxCore
+patchelf --set-rpath ./ $LIBRARY_INSTALL_DIR/libnvrtc.so
+patchelf --set-rpath ./ $LIBRARY_INSTALL_DIR/libnvrtc-builtins.so
