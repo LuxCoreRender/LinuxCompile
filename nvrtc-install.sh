6ecf06f7c48520e65e58b01af816fa80518d56c7
Simon Wendsche <simon.wendsche@gmx.de>
2020-08-05 00:29:45 +0200

    debugging why rpath fix is not packaged

--- nvrtc-install.sh
+++ nvrtc-install.sh
@@ -6,8 +6,17 @@ LIBRARY_INSTALL_DIR="$1"/lib
 mkdir -p $INCLUDE_INSTALL_DIR
 mkdir -p $LIBRARY_INSTALL_DIR
 
-cp -avr targets/x86_64-linux/lib/libnvrtc* $LIBRARY_INSTALL_DIR
+cp -avr targets/x86_64-linux/lib/libnvrtc* "$LIBRARY_INSTALL_DIR"
 
 # Fix missing rpath, so one shared lib can find the other when it is loaded by BlendLuxCore
-patchelf --set-rpath ./ $LIBRARY_INSTALL_DIR/libnvrtc.so
-patchelf --set-rpath ./ $LIBRARY_INSTALL_DIR/libnvrtc-builtins.so
+echo "Fixing NVRTC rpath"
+
+NVRTC="$LIBRARY_INSTALL_DIR"/libnvrtc.so
+patchelf --set-rpath ./ "$NVRTC"
+echo "new rpath of $NVRTC:"
+patchelf --print-rpath "$NVRTC"
+
+NVRTC_BUILTINS="$LIBRARY_INSTALL_DIR"/libnvrtc-builtins.so
+patchelf --set-rpath ./ "$NVRTC_BUILTINS"
+echo "new rpath of $NVRTC_BUILTINS:"
+patchelf --print-rpath "$NVRTC_BUILTINS"
