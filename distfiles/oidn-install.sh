f2c5694a893b215f37d3132a2705251c92d0df71
David Bucciarelli <dade916@gmail.com>
2021-04-14 15:24:25 +0200

    Updated for latest LinuxCompileDeps

--- distfiles/oidn-install.sh
+++ distfiles/oidn-install.sh
@@ -6,6 +6,10 @@ LIBRARY_INSTALL_DIR="$1"/lib
 mkdir -p $INCLUDE_INSTALL_DIR
 mkdir -p $LIBRARY_INSTALL_DIR
 
-cp -vr include/OpenImageDenoise $INCLUDE_INSTALL_DIR
+cp -av include/OpenImageDenoise $INCLUDE_INSTALL_DIR
 
-cp -vr lib/lib* $LIBRARY_INSTALL_DIR
+cp -av lib/lib* $LIBRARY_INSTALL_DIR
+
+rm $LIBRARY_INSTALL_DIR/libtbb.so
+rm $LIBRARY_INSTALL_DIR/libtbb.so.12
+cp $LIBRARY_INSTALL_DIR/libtbb.so.12.1 $LIBRARY_INSTALL_DIR/libtbb.so.12
